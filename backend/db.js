e583cb4ded0d349a48bd901dcac2ecdef36e5457
patilgreeshma <greeshma0614@gmail.com>
2026-06-16 10:30:19 +0530

    Update MongoDB connection

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/studentDB");
+    await mongoose.connect("MONGO_URI");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error);
